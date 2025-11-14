2c0c21c273afda6f0b956b24ca8d5fb291b7d235
Dogesh_badmosh ;) <navingulhane007@gmail.com>
2025-11-15 01:30:33 +0530

    Create switch-case.c with example and syntax
    Added a switch-case statement example in C.

---
+++ 01_Chapter/ Control_Statements.c/Selection_Statements/switch-case.c
@@ -0,0 +1,36 @@
+Switch-Case
+  
+📌 Definition
+
+The switch-case statement is used when you want to check one variable against many fixed values.
+It makes code cleaner than using many else-if statements.
+
+🧠 Syntax
+switch (variable) {
+    case value1:
+        // code
+        break;
+
+    case value2:
+        // code
+        break;
+
+    default:
+        // code for no match
+}
+
+📝 Example
+#include <stdio.h>
+
+int main() {
+    int day = 3;
+
+    switch (day) {
+        case 1: printf("Monday"); break;
+        case 2: printf("Tuesday"); break;
+        case 3: printf("Wednesday"); break;
+        default: printf("Invalid day");
+    }
+
+    return 0;
+}
