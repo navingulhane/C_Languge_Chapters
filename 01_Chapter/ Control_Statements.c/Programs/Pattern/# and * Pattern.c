a707642623ef83e4b68fa2d41d0b2bb2a765250a
---navin <navingulhane007@gmail.com>
2025-11-23 17:20:20 +0530

    Create # and * Pattern.c

---
+++ 01_Chapter/ Control_Statements.c/Programs/Pattern/# and * Pattern.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+int main (){
+
+    for ( int i = 1; i <= 4; i++){
+        printf(" #");
+    }
+    printf(" \n");
+
+    for( int i = 1; i <= 4; i ++){
+        printf(" *");
+    }
+    return 0;
+}
